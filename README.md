0abd783e71f2cced85c87be5998523584da43a8f
ubaid1900 <ubaid1900@gmail.com>
2020-10-27 08:39:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-iv6pgl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-iv6pgl)
---
+++ index.js
@@ -0,0 +1,34 @@
+var name_first = "";
+var name_last = "";
+
+// document.write(name_first);
+// document.write(name_last);
+
+var number1 = 6;
+var number2 = 9;
+
+// document.writeln(number1 + number2);
+// document.writeln(name_first + " " + name_last);
+// document.writeln(number1 + name_first);
+// document.writeln(name_last + 2);
+
+// var appDiv = document.getElementById("app");
+// document.writeln(appDiv);
+// console.log(appDiv);
+
+// var numbers = [1, 2, 3];
+// console.log(numbers);
+
+// var firstNames = ["John", "Tom"];
+// console.log(firstNames);
+
+// var firstNameTB = document.getElementById("firstName");
+// console.log(firstNameTB.value);
+// firstNameTB.value = "n";
+// console.log(firstNameTB.value);
+
+// var containers = document.getElementsByClassName("container");
+// console.log(containers);
+
+var textBoxes = document.getElementsByTagName("input");
+console.log(textBoxes);
